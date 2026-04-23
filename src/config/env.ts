a483b4903609addf666338e1480c56dc0384e06c
alsamiul123 <alsamiul123@gmail.com>
2026-04-23 19:58:38 +0600

    chore:project setup

--- src/config/env.ts
+++ src/config/env.ts
@@ -4,7 +4,7 @@ import { SignOptions } from "jsonwebtoken";
 export const config = {
   node_env: process.env.NODE_ENV,
   port: process.env.PORT,
-  db_url: process.env.DB_URL as string,
+  db_url: process.env.DB_URI as string,
   jwt_secret: process.env.JWT_SECRET as string,
   jwt_refresh_secret: process.env.JWT_REFRESH_SECRET as string,
   jwt_expires_in: process.env.JWT_EXPIRES_IN as SignOptions["expiresIn"],
